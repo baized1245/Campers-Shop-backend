2f46fdca8e14bc80a9a1849e6e460f4700b9333b
unknown <you@example.com>
2024-07-08 21:08:43 +0600

    Craete AppError function

---
+++ src/app/errors/AppError.ts
@@ -0,0 +1,19 @@
+// Custom error class extending the built-in Error class
+class AppError extends Error {
+    public statusCode: number;
+
+    //Constructor for creating an instance of AppError.
+    constructor(statusCode: number, message: string, stack = '') {
+        super(message);
+        this.statusCode = statusCode;
+
+        if (stack) {
+            this.stack = stack;
+        } else {
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
+}
+
+// Export the AppError class as the default export
+export default AppError;
