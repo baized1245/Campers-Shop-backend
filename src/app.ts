e6c99c1f5b9d066738ec52366aea4182a3538e48
unknown <you@example.com>
2024-07-08 20:30:12 +0600

    Campers Shop Backend

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+// import globalErrorHandler from './app/middlewares/globalErrorHandler';
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+// Root route
+app.get('/', (req: Request, res: Response) => {
+  res.send('Level 2 student, BH.Sadhin');
+});
+
+// application routes
+// app.use('/api',);
+// app.use('/api',);
+// app.use('/api',);
+
+// app.use(globalErrorHandler);
+
+// Handle not found route
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: 'Not Found',
+  });
+});
+
+export default app;
