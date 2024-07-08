20da3f9fd8783decf892e20fb1acc180e812b5c6
unknown <you@example.com>
2024-07-08 21:07:15 +0600

    Craete zod validation middleware

---
+++ src/app/middlewares/validateRequest.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from 'express';
+import { AnyZodObject } from 'zod';
+import catchAsync from '../utils/catchAsync';
+
+// Validation for incoming requests against a Zod schema.
+const validateRequest = (schema: AnyZodObject) => {
+    return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+        await schema.parseAsync({
+            body: req.body,
+        });
+
+        next();
+    });
+};
+
+// Exporting the validateRequest function
+export default validateRequest;
