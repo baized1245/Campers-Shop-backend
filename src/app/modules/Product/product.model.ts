c194119b184df8a810ef038062af68cfc3ba5c46
unknown <you@example.com>
2024-07-08 23:51:49 +0600

    Create product model

---
+++ src/app/modules/Product/product.model.ts
@@ -0,0 +1,17 @@
+import { Schema, model } from "mongoose";
+import { TProduct } from "./product.interface";
+
+// Create the schema for the TProduct model
+const productSchema: Schema<TProduct> = new Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  stock: { type: Number, required: true },
+  quantity: { type: Number, required: true },
+  description: { type: String, required: true },
+  category: { type: String, required: true },
+  ratings: { type: Number, required: true },
+  images: { type: String, required: true },
+});
+
+// Create the model from the schema
+export const Product = model<TProduct>("Product", productSchema);
