fe3bdd27100f6f56c3d01b380487350cabdf1961
unknown <you@example.com>
2024-07-08 20:59:48 +0600

    Create import { Response } from 'express';

---
+++ src/app/utils/noDataFoundResponse.ts
@@ -0,0 +1,30 @@
+import { Response } from 'express';
+import httpStatus from 'http-status';
+
+interface TResponseOptions {
+    success: boolean;
+    statusCode: number;
+    message: string;
+    data: any;
+}
+
+//Sends a  response for "Not Found" as per assigment required
+export const noFoundResponse = (res: Response, options: TResponseOptions) => {
+    const { success, statusCode, message, data } = options;
+    res.status(statusCode).json({
+        success,
+        statusCode,
+        message,
+        data,
+    });
+};
+
+//Sends a response with provided options.
+export const NotFoundResponse = (res: Response) => {
+    noFoundResponse(res, {
+        success: false,
+        statusCode: httpStatus.NOT_FOUND,
+        message: 'No Data Found',
+        data: [],
+    });
+};
